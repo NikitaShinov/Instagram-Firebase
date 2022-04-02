6097743d3879065249bb6eebde259875e10e528e
Nikita Shinov <shinov_96@mail.ru>
2022-04-02 18:05:03 +0300

    user profile header creation

--- Instagram Firebase/UserProfileController.swift
+++ Instagram Firebase/UserProfileController.swift
@@ -8,15 +8,31 @@
 import UIKit
 import Firebase
 
-class UserProfileController: UICollectionViewController {
+class UserProfileController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
     override func viewDidLoad() {
         super.viewDidLoad()
         
         collectionView.backgroundColor = .white
         
+//        navigationItem.title = Auth.auth().currentUser?.uid
+        
         fetchUser()
         
+        collectionView.register(UserProfileHeader.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "headerID")
+        
     }
+    
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "headerID", for: indexPath) as! UserProfileHeader
+        header.user = self.user
+        return header
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize(width: view.frame.width, height: 200)
+    }
+    
+    var user: User?
     
     fileprivate func fetchUser() {
         
@@ -26,11 +42,25 @@ class UserProfileController: UICollectionViewController {
             
             guard let dictionary = snapshot.value as? [String: Any] else { return }
             
-            let username = dictionary["username"] as? String
+            self.user = User(dictionary: dictionary)
             
-            self.navigationItem.title = username
+            self.navigationItem.title = self.user?.username
+            
+            self.collectionView.reloadData()
+            
         }, withCancel: { error in
             print (error.localizedDescription)
         } )
+    }
+}
+
+
+struct User {
+    let username: String
+    let profileImageUrl: String
+    
+    init(dictionary: [String: Any]) {
+        self.username = dictionary["username"] as? String ?? ""
+        self.profileImageUrl = dictionary["profileImageUrl"] as? String ?? ""
     }
 }
