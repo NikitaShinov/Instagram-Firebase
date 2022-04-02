3968b74fe4327572f9b6b0db1ce5c64beb047629
Nikita Shinov <shinov_96@mail.ru>
2022-04-02 20:04:24 +0300

    setting bottom toolbar

---
+++ Instagram Firebase/Profile/UserProfileHeader.swift
@@ -0,0 +1,110 @@
+//
+//  UserProfileHeader.swift
+//  Instagram Firebase
+//
+//  Created by max on 02.04.2022.
+//
+
+import UIKit
+import Firebase
+
+class UserProfileHeader: UICollectionViewCell {
+    
+    var user: User? {
+        didSet {
+            setupProfileImage()
+            usernameLabel.text = user?.username
+        }
+    }
+    
+    let profileImageView: UIImageView = {
+        let image = UIImageView()
+        return image
+    }()
+    
+    let gridButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(named: "grid"), for: .normal)
+        button.tintColor = UIColor(white: 0, alpha: 0.2)
+        return button
+    }()
+    
+    let listButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(named: "list"), for: .normal)
+        button.tintColor = UIColor(white: 0, alpha: 0.2)
+        return button
+    }()
+    
+    let bookmarkButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(named: "ribbon"), for: .normal)
+        button.tintColor = UIColor(white: 0, alpha: 0.2)
+        return button
+    }()
+    
+    let usernameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "username"
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+//        backgroundColor = .blue
+        
+        addSubview(profileImageView)
+        profileImageView.anchor(top: topAnchor, left: leftAnchor, bottom: nil, right: nil, paddingTop: 12, paddingLeft: 12, paddingBottom: 0, paddingRight: 0, width: 80, height: 80)
+        profileImageView.layer.cornerRadius = 80 / 2
+        profileImageView.clipsToBounds = true
+        
+        setupBottomToolBar()
+        addSubview(usernameLabel)
+        usernameLabel.anchor(top: profileImageView.bottomAnchor, left: leftAnchor, bottom: gridButton.topAnchor, right: rightAnchor, paddingTop: 4, paddingLeft: 12, paddingBottom: 0, paddingRight: 12, width: 0, height: 0)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    fileprivate func setupBottomToolBar() {
+        
+        let stackView = UIStackView(arrangedSubviews: [listButton, gridButton, bookmarkButton])
+        
+        addSubview(stackView)
+        
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        
+        stackView.anchor(top: nil, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 50)
+    }
+    
+    fileprivate func setupProfileImage() {
+        
+        print ("start loading")
+        
+        guard let profileImageUrl = user?.profileImageUrl else { return }
+        
+        guard let url = URL(string: profileImageUrl) else { return }
+        
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data, error == nil else { return }
+            
+            print ("loaded")
+            
+            let image = UIImage(data: data)
+            DispatchQueue.main.async {
+                print ("setting image")
+                self.profileImageView.image = image
+            }
+            
+        }.resume()
+        
+        
+    }
+    
+    
+}
