65c3f87c221c6da3780114ea75d013b2ada8d65d
Nikita Shinov <shinov_96@mail.ru>
2022-04-07 21:02:48 +0300

    custom transition implemented

--- Instagram Firebase/Camera/CameraController.swift
+++ Instagram Firebase/Camera/CameraController.swift
@@ -9,7 +9,7 @@ import UIKit
 import AVFoundation
 
 
-class CameraController: UIViewController, AVCapturePhotoCaptureDelegate {
+class CameraController: UIViewController, AVCapturePhotoCaptureDelegate, UIViewControllerTransitioningDelegate {
     
     let output = AVCapturePhotoOutput()
     
@@ -30,9 +30,25 @@ class CameraController: UIViewController, AVCapturePhotoCaptureDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        transitioningDelegate = self
+        
         setupCaptureSession()
         setupHUD()
         
+    }
+    
+    let customAnimationPresenter = CustomAnimationPresenter()
+    let customAnimationDismisser = CustomAnimationDismisser()
+    
+    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        
+        
+        return customAnimationPresenter
+    }
+    
+    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        
+        return customAnimationDismisser
     }
     
     override var prefersStatusBarHidden: Bool {
