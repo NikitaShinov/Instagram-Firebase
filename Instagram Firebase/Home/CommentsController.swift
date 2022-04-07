a91956f52587688343393cb280782db0fb997145
Nikita Shinov <shinov_96@mail.ru>
2022-04-08 02:00:21 +0300

    comments vc editing

--- Instagram Firebase/Home/CommentsController.swift
+++ Instagram Firebase/Home/CommentsController.swift
@@ -11,6 +11,55 @@ class CommentsController: UICollectionViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.title = "Comments"
+        
         collectionView?.backgroundColor = .red
+        
+        tabBarController?.tabBar.isHidden = true 
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tabBarController?.tabBar.isHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        tabBarController?.tabBar.isHidden = false
+    }
+    
+    var containerView: UIView = {
+        let containerView = UIView()
+        containerView.backgroundColor = .white
+        containerView.frame = CGRect(x: 0, y: 0, width: 100, height: 50)
+        
+        let submitButton = UIButton(type: .system)
+        submitButton.setTitle("Submit", for: .normal)
+        submitButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        submitButton.setTitleColor(.black, for: .normal)
+        submitButton.addTarget(self, action: #selector(submitComment), for: .touchUpInside)
+        containerView.addSubview(submitButton)
+        submitButton.anchor(top: containerView.topAnchor, left: nil, bottom: containerView.bottomAnchor, right: containerView.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 12, width: 50, height: 0)
+        
+        let textField = UITextField()
+        textField.placeholder = "Enter comment"
+        containerView.addSubview(textField)
+        textField.anchor(top: containerView.topAnchor, left: containerView.leftAnchor, bottom: containerView.bottomAnchor, right: submitButton.leftAnchor, paddingTop: 0, paddingLeft: 12, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+        return containerView
+    }()
+    
+    @objc private func submitComment() {
+        print ("submitting comment")
+    }
+    
+    override var inputAccessoryView: UIView? {
+        get {
+            return containerView
+        }
+    }
+    
+    override var canBecomeFirstResponder: Bool {
+        return true
     }
 }
